1724b5b3f46ccc89e34d4cf050c9c083d16a9512
Charles Adeyinka <55859386+Kaykai-19@users.noreply.github.com>
2021-07-06 00:39:46 -0400

    Delete index.js

--- Scripts/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-  var anchors = document.querySelectorAll('a');
-  for (var i=0; i<anchors.length; i++){
-    anchors[i].setAttribute('target', '_blank');
-    anchors[i].setAttribute('rel', "noopener noreferrer")
-  }
-}
